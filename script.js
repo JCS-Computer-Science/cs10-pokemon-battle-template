5a2445a892680f221ff3af1a897276cc8d706458
JoeTodes <jtwilson.home@gmail.com>
2022-05-16 10:03:49 -0600

    level 1.5 solutions

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 var gengarHP = 60;
 var nidorinoHP = 61;
+var isPlayerTurn = true;
+
+var message = document.getElementById("message");
+var opponentHPElement = document.getElementById("opponent-hp");
+opponentHPElement.innerHTML = nidorinoHP;
+
+var playerHPElement = document.getElementById("player-hp");
+playerHPElement.innerHTML = gengarHP;
+
+function tackle() {
+	if (isPlayerTurn == true) {
+		nidorinoHP -= 5;
+		if (nidorinoHP <= 0) {
+			nidorinoHP = 0;
+			message.innerHTML = "NIDORINO fainted!";
+		}
+
+		opponentHPElement.innerHTML = nidorinoHP;
+	} else {
+		gengarHP -= 5;
+		if (gengarHP <= 0) {
+			gengarHP = 0;
+			message.innerHTML = "GENGAR fainted!";
+		}
+
+		playerHPElement.innerHTML = gengarHP;
+	}
+	isPlayerTurn = !isPlayerTurn;
+}
